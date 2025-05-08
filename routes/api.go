695e6a882c3374cdab0e3a5a577d6f9711b430b4
Alif-Fikri <aliefmaulanafikri@gmail.com>
2025-05-08 16:22:53 +0700

    update route

--- routes/api.go
+++ routes/api.go
@@ -15,6 +15,19 @@ func Api(r *gin.Engine) {
 	api.POST("/wali-kelas/login", tc.LoginWaliKelas)
 	api.POST("/logout", middlewares.AuthMiddleware(), tc.Logout)
 
+	api.GET("/dashboard-guru", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("guru"), func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dashboard Guru"})
+	})
+
+	api.GET("/dashboard-admin", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"), func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dashboard Admin"})
+	})
+
+	api.GET("/dasboard-walikelas", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("wali_kelas"), func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "dashboard Wali Kelas"})
+
+	})
+
 	guru := api.Group("/guru")
 	guru.Use(middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"))
 	{
@@ -23,18 +36,34 @@ func Api(r *gin.Engine) {
 		guru.POST("/", tc.CreateGuru)
 		guru.PUT("/:id", tc.UpdateGuru)
 		guru.DELETE("/:id", tc.DeleteGuru)
-	}
 
-	api.GET("/dashboard-guru", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("guru"), func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "dashboard Guru"})
-	})
+	}
 
 	waliKelas := api.Group("/wali-kelas")
 	waliKelas.Use(middlewares.AuthMiddleware(), middlewares.RoleMiddleware("wali_kelas"))
 	{
-		waliKelas.GET("/kelas", tc.GetKelasWaliKelas)   
-		waliKelas.GET("/siswa", tc.GetSiswaByWaliKelas) 
+		waliKelas.GET("/kelas", tc.GetKelasWaliKelas)
+		waliKelas.GET("/siswa", tc.GetSiswaByWaliKelas)
 		waliKelas.GET("/daftar", tc.GetAllWaliKelas)
+	}
+
+	kelas := api.Group("/kelas")
+	kelas.Use(middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"))
+	{
+		kelas.POST("/", tc.CreateKelas)
+	}
+
+	mapel := api.Group("/mapel")
+	mapel.Use(middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"))
+	{
+		mapel.POST("/", tc.CreateMataPelajaran)
+	}
+
+	siswa := api.Group("/siswa")
+	siswa.Use(middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"))
+	{
+		siswa.POST("/", tc.CreateSiswa)
+		siswa.GET("/", tc.GetSiswaByKelas)
 	}
 
 }
