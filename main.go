c8389d75f2f180fc8d1891edfbe465592c8ba256
Alif-Fikri <aliefmaulanafikri@gmail.com>
2025-06-09 18:53:24 +0700

    setup firebase

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"abs-be/database"
 	"abs-be/routes"
+	"abs-be/utils"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -14,6 +15,7 @@ func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
 	routes.Api(r)
+	utils.InitializeFirebase()
 
 	if err := r.Run(":8080"); err != nil {
 		panic("gagal menjalankan server: " + err.Error())
